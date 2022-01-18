21c3b17ef01f00eef29a523a2ec512e9d0ae600d
sarit-chaet-hudis <sarit.chh@gmail.com>
2022-01-18 13:22:43 +0200

    update

--- blogserver.js
+++ blogserver.js
@@ -14,12 +14,17 @@ client.connect(async (err) => {
 
   const db = client.db();
 
-  const result = await db.collection("users").insertOne({
-    name: "Tamar CY",
-    email: "tamarcy@gmail.com",
-    avatar: "thispersondoesnotexist.com/image.jpg",
+  const result = await db.collection("posts").insertOne({
+    title: "How I met your father",
+    author: "61e696c83d31ac332cf8e7ed",
+    content: "lorem ipsum dolermo sit yanis",
+    comments: [
+      { text: "please expand", author: "61e69c9a86d348087d0ca446" },
+      { text: "thank you!", author: "61e696c83d31ac332cf8e7ed" },
+    ],
   });
-  console.log(`A user was added with _id ${result.insertedId}`);
+  console.log(`A post was added with _id ${result.insertedId}`);
+  console.log(`at ${result.insertedId.getTimestamp()}`);
 
   client.close();
 });
